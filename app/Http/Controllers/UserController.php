e7df0706c09ea6c792c612d87ef4395980c8e11c
Chihhung Jen <jenchihhung7724@gmail.com>
2023-09-10 20:25:44 +0800

    User not an admin can only edit or delete own data

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -21,14 +22,27 @@ class UserController extends Controller
 
     public function update(Request $request)
     {
+
+
+
         $id = $request->id;
         if($id == null)  return redirect()->back();
+
+
+        //如果不是管理者，無法修改別人的資料
+        $loggedInUser = Auth::user();
+        if($loggedInUser->role != "admin" && $loggedInUser->id != $id) return redirect()->back();
+
+
 
         User::findOrFail($id)->update([
             'email' => $request->email,
             'phone' => $request->phone,
         ]);
     
+
+
+
 
         return redirect()->back();
     }
@@ -41,6 +55,11 @@ class UserController extends Controller
   
     public function delete(string $id)
     {   
+
+         //如果不是管理者，無法刪除別人的資料
+        $loggedInUser = Auth::user();
+        if($loggedInUser->role != "admin" && $loggedInUser->id != $id) redirect()->route('dashboard');
+
 
         User::findOrFail($id)->delete();
         return redirect()->route('dashboard');
